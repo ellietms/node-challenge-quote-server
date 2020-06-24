9824489c360e84a1f1906f577f65f5bc16affd20
ellie <ellie.tms1371@gmail.com>
2020-06-24 17:12:11 +0100

    add my codes

--- server.js
+++ server.js
@@ -1,34 +1,28 @@
-// server.js
-// This is where your node app starts
-
-//load the 'express' module which makes writing webservers easy
 const express = require("express");
 const app = express();
-
-//load the quotes JSON
 const quotes = require("./quotes.json");
 
-// Now register handlers for some routes:
-//   /                  - Return some helpful welcome info (text)
-//   /quotes            - Should return all quotes (json)
-//   /quotes/random     - Should return ONE quote (json)
-app.get("/", function (request, response) {
-  response.send("Neill's Quote Server!  Ask me for /quotes/random, or /quotes");
+app.get("/",function (request, response) {
+  response.send("Welcome to Ellie 's first back-end app")
+})
+
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
 });
 
-//START OF YOUR CODE...
+app.get("/quotes/random",function (request, response) {
+  response.send(pickFromArray(quotes))
+})
 
-//...END OF YOUR CODE
+app.post("/favorite", function (req, res) {
+  console.log(req);
+  res.send(`My favorite is ${req.query.family}`);
+});
 
-//You can use this function to pick one element at random from a given array
-//example: pickFromArray([1,2,3,4]), or
-//example: pickFromArray(myContactsArray)
-//
 function pickFromArray(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
-//Start our server so that it listens for HTTP requests!
 const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
