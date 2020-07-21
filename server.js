31601ff28ae8804b8fdd770fa60c5b99dddbfce5
ellie <ellie.tms1371@gmail.com>
2020-07-21 22:38:07 +0100

    tidy up

--- server.js
+++ server.js
@@ -8,15 +8,13 @@ const cors = require("cors");
 app.use(cors());
 const lodash = require("lodash");
 
-
-
 // Level 100
 app.get("/", function (request, response) {
-  const client = new mongodb.MongoClient(uri,{ useNewUrlParser: true });
-  client.connect(() =>{
-  response.send("Welcome to Ellie 's quote app");
-  client.close();
-  })
+  const client = new mongodb.MongoClient(uri, { useNewUrlParser: true });
+  client.connect(() => {
+    response.send("Welcome to Ellie 's quote app");
+    client.close();
+  });
 });
 
 app.get("/quotes", function (request, response) {
@@ -28,51 +26,53 @@ app.get("/quotes", function (request, response) {
       response.json(quotes || error);
       client.close();
     });
-    })
-  })
+  });
+});
 
 app.get("/quotes/random", function (request, response) {
   const client = mongodb.MongoClient(uri);
   client.connect(() => {
     const db = client.db("quotes");
     const collection = db.collection("quotes");
-    collection.find().toArray((error,quotes) => {
+    collection.find().toArray((error, quotes) => {
       response.send(lodash.sample(quotes) || error);
       client.close();
-    })
-  })
+    });
+  });
 });
-
-
 
 // Level 200
 app.post("/quotes/search", function (request, response) {
-  const client = new  mongodb.MongoClient(uri,{ useUnifiedTopology: true });
+  const client = new mongodb.MongoClient(uri, { useUnifiedTopology: true });
   client.connect(() => {
     const db = client.db("quotes");
     const collection = db.collection("quotes");
-    collection.find().toArray((error,quotes) => {
+    collection.find().toArray((error, quotes) => {
       const term = request.query.term;
       console.log(word);
-      const filteredQuotes = quotes.filter(eachQuote => eachQuote.quote.includes(term))
+      const filteredQuotes = quotes.filter((eachQuote) =>
+        eachQuote.quote.includes(term)
+      );
       response.send(error || filteredQuotes);
       client.close();
-    })
-  })
+    });
+  });
 });
 
-app.post("/quotes/search/echo", function (request, response){
+app.post("/quotes/search/echo", function (request, response) {
   const client = new mongodb.MongoClient(uri);
   client.connect(() => {
     const db = client.db("quotes");
     const collection = db.collection("quotes");
-    collection.find().toArray((error,quotes) => {
+    collection.find().toArray((error, quotes) => {
       const word = request.query.word;
-      const filteredQuotes = quotes.filter(eachQuote => eachQuote.quote.includes(word));
+      const filteredQuotes = quotes.filter((eachQuote) =>
+        eachQuote.quote.includes(word)
+      );
       response.json(error || filteredQuotes);
       client.close();
-    })
-  })
+    });
+  });
 });
 
 const listener = app.listen(3000, function () {
@@ -80,5 +80,3 @@ const listener = app.listen(3000, function () {
 });
 // const port = process.env.PORT || 5000;
 // app.listen(port);
-
-
